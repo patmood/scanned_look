69e7ae3b481a4450b4da06d5fc0a975be4e93f66
patmood <patrick.n.moody@gmail.com>
2013-11-06 10:55:16 +1030

    refactor

--- scanned.rb
+++ scanned.rb
@@ -13,45 +13,37 @@ class Scanned < Sinatra::Base
   end
 
   post "/upload" do
-    raise "Must be a PDF" unless params['myfile'][:type] == 'application/pdf'
+    p params
     tempfile = params['myfile'][:tempfile]
     filename = params['myfile'][:filename]
     filename.gsub!(/\s/,"") if /\s/.match(filename)
+    filename_stripped = filename.gsub(/\.\w+\z/,"")
     FileUtils.copy(tempfile.path, "uploads/#{filename}")
 
-
+    # Count pages
     pages = PDF::Reader.new("uploads/#{filename}").page_count
     page_paths = []
 
     # "Scan" each page
     pages.times do |page|
-      system("convert #{Dir.pwd}/uploads/#{filename}[#{page}] -mattecolor gray99 -frame 1x1+1 -colorspace gray \\( +clone -blur 0x1 \\) +swap -compose divide -resize 800 -composite -contrast-stretch 5%,0% -rotate #{rand*2 - 1} #{Dir.pwd}/converted/#{filename}-#{page}-scanned.pdf")
-      page_paths << "#{Dir.pwd}/converted/#{filename}-#{page}-scanned.pdf"
+      system("convert #{Dir.pwd}/uploads/#{filename}[#{page}] -mattecolor gray99 -frame 1x1+1 -colorspace gray \\( +clone -blur 0x1 \\) +swap -compose divide -resize 800 -composite -contrast-stretch 5%,0% -rotate #{rand*2 - 1} #{Dir.pwd}/converted/#{filename_stripped}-#{page}-scanned.pdf")
+      page_paths << "#{Dir.pwd}/converted/#{filename_stripped}-#{page}-scanned.pdf"
     end
 
-    # Combine into single PDF
-    system("gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=#{Dir.pwd}/converted/#{filename}-scanned.pdf #{page_paths.join(' ')}")
+    # Combine pages into single PDF
+    system("gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=#{Dir.pwd}/converted/#{filename_stripped}-scanned.pdf #{page_paths.join(' ')}")
 
     # Delete original upload
     File.delete("#{Dir.pwd}/uploads/#{filename}")
 
     # Delete scanned pages
     pages.times do |page|
-      File.delete("#{Dir.pwd}/converted/#{filename}-#{page}-scanned.pdf")
+      File.delete("#{Dir.pwd}/converted/#{filename_stripped}-#{page}-scanned.pdf")
     end
 
-    # return "The file was successfully scanned! <a href='download/#{filename}-scanned.pdf'>Download!</a>"
-    redirect "download/#{filename}-scanned.pdf"
+    return "The file was successfully scanned! <a href='download/#{filename_stripped}-scanned.pdf'>Download!</a>"
+    # redirect "download/#{filename_stripped}-scanned.pdf"
 
 
   end
-
-
-  # convert test.pdf -mattecolor gray99 -frame 1x1+1 -colorspace gray \( +clone -blur 0x1 \) +swap -compose divide -resize 800 -composite -contrast-stretch 5%,0% -rotate 1 as-scanned.jpg
-  # Merge PDFs in directory with ghostscript
-  # gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=temp.pdf pdf1.pdf pdf2.pdf
-
-  # Run sinatra on digital ocean
-  # http://michaelcarrano.com/blog/deploying-sinatra-app-on-digitalocean-with-nginx-and-thin
-
 end
