3626ec841f53da35a5b9f1d00eae088834913529
patmood <patrick.n.moody@gmail.com>
2014-04-05 19:50:17 -0700

    switch to formidable for uploads

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ app.use(express.logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.methodOverride())
-app.use(express.bodyParser({
-  uploadDir: __dirname + '/temp'
-}))
 app.use(app.router)
 app.use(express.static(path.join(__dirname, 'public')))
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var fs = require('fs')
+  , formidable = require('formidable')
   , path = require('path')
   , mime = require('mime')
 
@@ -7,18 +8,22 @@ exports.index = function(req, res){
 }
 
 exports.upload = function(req, res){
-  console.log(req.files)
+  var form = new formidable.IncomingForm()
 
-  var file = req.files.scanMe.path
-    , filename = path.basename(file)
-    , mimetype = mime.lookup(file)
-    , filestream = fs.createReadStream(file)
+  form.parse(req, function(err, fields, files) {
+    if (err) throw err
+    console.log(files)
 
-  res.setHeader('Content-disposition', 'attachment; filename=' + filename)
-  res.setHeader('Content-type', mimetype)
+    var file = files.scanMe.path
+      , filename = path.basename(file)
+      , mimetype = mime.lookup(file)
+      , filestream = fs.createReadStream(file)
 
-  filestream.pipe(res)
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename)
+    res.setHeader('Content-type', mimetype)
+
+    filestream.pipe(res)    
+
+  })
 }
 
-// Be careful allowing uploads
-// https://groups.google.com/forum/#!msg/express-js/iP2VyhkypHo/5AXQiYN3RPcJ
