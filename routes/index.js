b01523ed585e2fefe04f463658fc96718fb3d1c1
patmood <patrick.n.moody@gmail.com>
2014-04-07 22:57:45 -0700

    ability to scan 1 page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var fs = require('fs')
   , formidable = require('formidable')
   , path = require('path')
   , mime = require('mime')
+  , exec = require('child_process').exec
 
 exports.index = function(req, res){
   res.render('index', { title: 'Scanned Look' })
@@ -9,7 +10,6 @@ exports.index = function(req, res){
 
 exports.upload = function(req, res){
   var form = new formidable.IncomingForm()
-  form.keepExtensions = true
 
   form.parse(req, function(err, fields, files) {
     if (err) throw err
@@ -18,13 +18,19 @@ exports.upload = function(req, res){
     var file = files.scanMe.path
       , filename = path.basename(file)
       , mimetype = mime.lookup(file)
-      , filestream = fs.createReadStream(file)
+      , result = file + '_scan.pdf'
 
-    res.setHeader('Content-disposition', 'attachment; filename=' + filename)
-    res.setHeader('Content-type', mimetype)
-
-    filestream.pipe(res)    
-
+    exec('convert ' 
+      + file 
+      + ' -mattecolor gray99 -frame 1x1+1 -colorspace gray \\( +clone -blur 0x1 \\) +swap -compose divide -resize 800 -composite -contrast-stretch 5%,0% -rotate '
+      + (Math.random() - 0.5)
+      + ' ' 
+      + result
+    , function(err, stout, sterr){
+      res.setHeader('Content-disposition', 'attachment; filename=' + filename)
+      res.setHeader('Content-type', mimetype)
+      fs.createReadStream(result).pipe(res)    
+    })
   })
 }
 
