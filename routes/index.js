11a99533098a44f7f3f448496e2536a19f6b1104
patmood <patrick.n.moody@gmail.com>
2014-04-12 20:50:10 -0700

    scan the individual pages

--- routes/index.js
+++ routes/index.js
@@ -10,27 +10,69 @@ exports.index = function(req, res){
 
 exports.upload = function(req, res){
   var form = new formidable.IncomingForm()
-
   form.parse(req, function(err, fields, files) {
     if (err) throw err
-    console.log(files)
+    
+    var file = files.scanMe
+      , filename = files.scanMe.name.replace(/\.\w+$/i, '')
+      , mimetype = files.scanMe.type 
 
-    var file = files.scanMe.path
-      , filename = path.basename(file)
-      , mimetype = mime.lookup(file)
-      , result = file + '_scan.pdf'
+    console.log('File Path:', file.path)
+    console.log('File Type:', mimetype)
 
-    exec('convert ' 
-      + file 
-      + ' -mattecolor gray99 -frame 1x1+1 -colorspace gray \\( +clone -blur 0x1 \\) +swap -compose divide -resize 800 -composite -contrast-stretch 5%,0% -rotate '
-      + (Math.random() - 0.5)
-      + ' ' 
-      + result
-    , function(err, stout, sterr){
-      res.setHeader('Content-disposition', 'attachment; filename=' + filename)
-      res.setHeader('Content-type', mimetype)
-      fs.createReadStream(result).pipe(res)    
+    if (!mimetype === 'application/pdf') {
+      console.log('MUST BE PDF!')
+      return
+    }
+
+    // Callback Hell
+    numPages(file, function(pageCount) {
+      var scannedPages = []
+
+      !function loop(i) {
+        if (i < pageCount) {
+          scan(file, i, function(result) {
+            scannedPages.push(result)
+            loop(++i)
+          })
+        } else {
+          console.log(scannedPages)
+          res.send('woo')
+        }
+      }(0)
+
+//      res.setHeader('Content-disposition', 'attachment; filename=' + path.basename(result))
+//      res.setHeader('Content-type', 'application/pdf')
+//      fs.createReadStream(file.path).pipe(res)    
     })
   })
 }
 
+var numPages = function(file, next){
+  exec('pdftk ' + file.path + ' dump_data | grep NumberOfPages | sed "s/[^0-9]*//"'
+  , function(err, stdout, stderr){
+    if (err) throw err
+    if (stderr) console.log("Error counting pages:", stderr)
+    console.log("Number of Pages:", stdout)
+    if (next) next(stdout)
+    return stdout
+  })
+}
+
+var scan = function(file, i, next){
+  var result = file.name.replace(/\.\w+$/i, '') + '_' + i + '_scan.pdf'
+  console.log('RESULT:',result)
+  exec('convert ' 
+    + file.path + '[' + i + ']'
+    + ' -mattecolor gray99 -frame 1x1+1 -colorspace gray '
+    + '\\( +clone -blur 0x1 \\) +swap -compose divide '
+    + '-resize 800 -composite -contrast-stretch 5%,0% -rotate '
+    + (Math.random() - 0.5)
+    + ' ' 
+    + result
+  , function(err, stout, sterr){
+    if (err) throw err
+    if (next) next(result)
+    return result
+  })
+}
